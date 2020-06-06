af5a3a820f50ff2c36e7706d3707645c230f5780
Nick Shu <nicolas.s.shu@gmail.com>
2020-06-05 23:18:13 -0400

    Wrote a prepping script

---
+++ prep_proj.sh
@@ -0,0 +1,18 @@
+# Test to see if no arguments were passed
+if [[ $# -eq 0 ]] ; then
+    echo "You need to pass a location to create a presentation folder"
+    exit 0
+fi
+
+# Obtain the project name
+projname=$1
+
+# Create the project
+mkdir $projname
+
+
+cp compile_beamer.sh $projname
+cp compile_revealjs.sh $projname
+cp example.md $projname/slides.md
+cp -r reveal.js $projname
+cp -r custom_styles $projname
